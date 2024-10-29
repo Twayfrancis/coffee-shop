d1b3955c35ea00fdd36326e1f4ea6e04839f84cf
Twayfrancis <tawaifrancis@gmail.com>
2024-10-30 01:18:50 +0300

    update login page

--- auth/login.php
+++ auth/login.php
@@ -1,38 +1,40 @@
 <?php require "../config/config.php"; ?>
 <?php require "../includes/header.php"; ?>
-
 <?php 
   if(isset($_POST['submit'])) {
-    if(empty($_POST['username']) OR empty($_POST['email']) OR empty($_POST['password'])) {
+    if(empty($_POST['email']) OR empty($_POST['password'])) {
       echo "<script>alert('one or more inputs are empty');</script>";
     } else {
-      $username = $_POST['username'];
       $email = $_POST['email'];
-      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+      $password = $_POST['password'];
 
-      $insert = $conn->prepare("INSERT INTO users (username, email, password)
-        VALUES (:username, :email, :password)");
+      $login = $conn->query("SELECT * FROM users WHERE email='$email'");
+      $login->execute();
 
-      $insert->execute([
-        ":username" => $username,
-        ":email" => $email,
-        ":password" => $password,
-      ]);
+      $fetch = $login->fetch(PDO::FETCH_ASSOC);
 
-      header("location: login.php");
+      if($login->rowCount() > 0) {
+        if(password_verify($password, $fetch['password'])) {
+          header("location: ".APPURL."");
+        } else {
+          echo "<script>alert('email or password is wrong');</script>";
+        }
+      } else {
+        echo "<script>alert('email or password is wrong');</script>";
+      }
     }
   }
 ?>
     <section class="home-slider owl-carousel">
 
-      <div class="slider-item" style="background-image: url(<?php echo APPURL; ?>/images/bg_2.jpg);" data-stellar-background-ratio="0.5">
+      <div class="slider-item" style="background-image: url(<?php echo APPURL; ?>/images/bg_1.jpg);" data-stellar-background-ratio="0.5">
       	<div class="overlay"></div>
         <div class="container">
           <div class="row slider-text justify-content-center align-items-center">
 
             <div class="col-md-7 col-sm-12 text-center ftco-animate">
-            	<h1 class="mb-3 mt-5 bread">Register</h1>
-	            <p class="breadcrumbs"><span class="mr-2"><a href="<?php echo APPURL; ?>/index.php">Home</a></span> <span>Register</span></p>
+            	<h1 class="mb-3 mt-5 bread">Login</h1>
+	            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Login</span></p>
             </div>
 
           </div>
@@ -44,16 +46,10 @@
       <div class="container">
         <div class="row">
           <div class="col-md-12 ftco-animate">
-			<form action="register.php" method="POST" class="billing-form ftco-bg-dark p-3 p-md-5">
-				<h3 class="mb-4 billing-heading">Register</h3>
+			<form action="login.php" method="POST" class="billing-form ftco-bg-dark p-3 p-md-5">
+				<h3 class="mb-4 billing-heading">Login</h3>
 	          	<div class="row align-items-end">
-                 <div class="col-md-12">
-                        <div class="form-group">
-                            <label for="Username">Username</label>
-                            <input type="text" name="username" class="form-control" placeholder="Username">
-                        </div>
-                 </div>
-	          	  <div class="col-md-12">
+	          		<div class="col-md-12">
 	                <div class="form-group">
 	                	<label for="Email">Email</label>
 	                  <input type="text" name="email" class="form-control" placeholder="Email">
@@ -70,7 +66,7 @@
                 <div class="col-md-12">
                 	<div class="form-group mt-4">
 							<div class="radio">
-                <button type="submit" name="submit" class="btn btn-primary py-3 px-4">Register</button>
+                  <button type="submit" name="submit" class="btn btn-primary py-3 px-4">Login</button>
 						    </div>
 					</div>
                 </div>
