1224839465ffd5289f772ef0336113e846b4cbb0
Twayfrancis <tawaifrancis@gmail.com>
2024-10-30 00:12:00 +0300

    update register.php

--- includes/footer.php
+++ includes/footer.php
@@ -17,7 +17,7 @@
             <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Recent Blog</h2>
               <div class="block-21 mb-4 d-flex">
-                <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
+                <a class="blog-img mr-4" style="background-image: url(<?php echo APPURL; ?>/images/image_1.jpg);"></a>
                 <div class="text">
                   <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
                   <div class="meta">
@@ -28,7 +28,7 @@
                 </div>
               </div>
               <div class="block-21 mb-4 d-flex">
-                <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
+                <a class="blog-img mr-4" style="background-image: url(<?php echo APPURL; ?>/images/image_2.jpg);"></a>
                 <div class="text">
                   <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
                   <div class="meta">
@@ -81,23 +81,23 @@
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/jquery.waypoints.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.animateNumber.min.js"></script>
-  <script src="js/bootstrap-datepicker.js"></script>
-  <script src="js/jquery.timepicker.min.js"></script>
-  <script src="js/scrollax.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/popper.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/bootstrap.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery.easing.1.3.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery.waypoints.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery.stellar.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/owl.carousel.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery.magnific-popup.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/aos.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery.animateNumber.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/bootstrap-datepicker.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery.timepicker.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/scrollax.min.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="js/google-map.js"></script>
-  <script src="js/main.js"></script>
+  <script src="<?php echo APPURL; ?>/js/google-map.js"></script>
+  <script src="<?php echo APPURL; ?>/js/main.js"></script>
 
     
   </body>
--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,7 @@
+<?php
+  define("APPURL", "http://localhost/coffee-blend");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,43 +13,43 @@
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/animate.css">
     
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/magnific-popup.css">
 
-    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/aos.css">
 
-    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/ionicons.min.css">
 
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.timepicker.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/jquery.timepicker.css">
 
     
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/flaticon.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/icomoon.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/style.css">
   </head>
   <body>
   	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-	      <a class="navbar-brand" href="index.html">Coffee<small>Blend</small></a>
+	      <a class="navbar-brand" href="<?php echo APPURL; ?>/index.php">Coffee<small>Blend</small></a>
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 			<ul class="navbar-nav ml-auto">
-				<li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-				<li class="nav-item"><a href="menu.html" class="nav-link">Menu</a></li>
-				<li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
-				<li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+				<li class="nav-item active"><a href="<?php echo APPURL; ?>/index.php" class="nav-link">Home</a></li>
+				<li class="nav-item"><a href="<?php echo APPURL; ?>/menu.html" class="nav-link">Menu</a></li>
+				<li class="nav-item"><a href="<?php echo APPURL; ?>/services.html" class="nav-link">Services</a></li>
+				<li class="nav-item"><a href="<?php echo APPURL; ?>/about.html" class="nav-link">About</a></li>
 			   
-				<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-				<li class="nav-item cart"><a href="cart.html" class="nav-link"><span class="icon icon-shopping_cart"></span></a>
-				<li class="nav-item"><a href="login.html" class="nav-link">login</a></li>
-				<li class="nav-item"><a href="register.html" class="nav-link">register</a></li>
+				<li class="nav-item"><a href="<?php echo APPURL; ?>/contact.html" class="nav-link">Contact</a></li>
+				<li class="nav-item cart"><a href="<?php echo APPURL; ?>/cart.html" class="nav-link"><span class="icon icon-shopping_cart"></span></a>
+				<li class="nav-item"><a href="<?php echo APPURL; ?>/login.html" class="nav-link">login</a></li>
+				<li class="nav-item"><a href="<?php echo APPURL; ?>/auth/register.php" class="nav-link">register</a></li>
   
 			  </ul>
 	      </div>
